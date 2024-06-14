157f2563912ec2a171f10166eb0cd76a9a92aabd
Shyngys03 <vip.sh3541@gmail.com>
2024-06-14 15:37:01 +0500

    lab 4

--- lab 4/online-store/src/data.js
+++ lab 4/online-store/src/data.js
@@ -19,7 +19,94 @@ export const products = [
         'https://m.media-amazon.com/images/I/61q9hmplGFL._AC_SL1500_.jpg',
         'https://m.media-amazon.com/images/I/61nkWvxSW+L._AC_SL1500_.jpg',
         'https://m.media-amazon.com/images/I/714r3QENpoL._AC_SL1500_.jpg'
-
+      ]
+    },
+    {
+      asin: 'B08DJBDG5L',
+      name: 'Sengled Alexa Light Bulb, S1 Auto Pairing with Alexa Devices, Warm Smart Light Bulbs, Bluetooth Mesh Smart Home Lighting, E26 60W Equivalent, 800LM, 1-Pack',
+      description: 'Sengled S1 Auto Paring: (No registration required, No 3p account required) The Sengled Auto Pairing Kit offers a simple setup and stable links. Just twist in the smart bulbs and start using them with Alexa Devices in seconds.',
+      rating: 4.4,
+      images: [
+        'https://m.media-amazon.com/images/I/71NEfdX5eSL._SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/81sMvah7l2L._SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/81h-w+pf1dL._SL1500_.jpg'
+      ]
+    },
+    {
+      asin: 'B0BZC1T9FJ',
+      name: 'Sengled Alexa Light Bulb, BR30, S1 Auto Pairing with Alexa Devices, Smart Flood Light Bulb That Work with Alexa, Daylight 5000K, E26, Led Lights, 65W Equivalent Recessed, No Hub Required, 2-Pack',
+      description: 'Sengled S1 Auto Paring: (No registration required, No 3p account required) The Sengled Auto Pairing Kit offers a simple setup and stable links. Just twist in the smart bulbs and start using them with Alexa Devices in seconds.',
+      rating: 4.1,
+      images: [
+        'https://m.media-amazon.com/images/I/51YQa1QeBYL._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/71uzqUVwS8L._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/615ZnM94fzL._AC_SL1500_.jpg'
+      ]
+    },
+    {
+      asin: 'B098LG3N6R',
+      name: 'MageGee Portable 60% Mechanical Gaming Keyboard, MK-Box LED Backlit Compact 68 Keys Mini Wired Office Keyboard with Red Switch for Windows Laptop PC Mac - Black/Grey',
+      description: 'Mini portable 60% compact layout: MK-Box is a 68 keys mechanical keyboard have cute small size, separate arrow keys and all your F-keys you need, can use it for gaming or work while saving space.',
+      rating: 4.4,
+      images: [
+        'https://m.media-amazon.com/images/I/618zZ7u3sUL._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/71Ju3yFqgjL._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/71VbXAHUnEL._AC_SL1500_.jpg'
+      ]
+    },
+    {
+      asin: 'B094PS5RZQ',
+      name: 'Razer DeathAdder Essential Gaming Mouse: 6400 DPI Optical Sensor - 5 Programmable Buttons - Mechanical Switches - Rubber Side Grips - Classic Black',
+      description: 'HIGH-PRECISION 6,400 DPI OPTICAL SENSOR — Offers on-the-fly sensitivity adjustment through dedicated DPI buttons (reprogrammable) for gaming and creative work',
+      rating: 4.4,
+      images: [
+        'https://m.media-amazon.com/images/I/8189uwDnMkL._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/61Vbk62PIjS._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/61V3921F10S._AC_SL1500_.jpg'
+      ]
+    },
+    {
+      asin: 'B09X6GJ691',
+      name: 'Razer Ornata V3 X Gaming Keyboard: Low-Profile Keys - Silent Membrane Switches - Spill Resistant - Chroma RGB Lighting - Ergonomic Wrist Rest - Classic Black',
+      description: 'LOW-PROFILE KEYS — With slimmer keycaps and shorter switches, enjoy natural hand positioning that allows for long hours of use with little strain',
+      rating: 4.4,
+      images: [
+        'https://m.media-amazon.com/images/I/71pqfvJKW5L._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/718G+rJjo0L._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/81QvouU5ciL._AC_SL1500_.jpg'
+      ]
+    },
+    {
+      asin: 'B005SPESKK',
+      name: 'Aveeno Baby Bathtime Solutions Baby & Me Gift Set with Baby Wash & Shampoo, Calming Baby Bath, Baby Daily Moisturizing Lotion & Stress Relief Body Wash for Mom, Oatmeal, 4 Items',
+      description: '4-piece Aveeno Baby Daily Bathtime Solutions Baby & Me Gift Set is a collection of baby bath essentials in a convenient, reusable wicker basket that includes everything you need for bath time with your little one, plus a bonus for mom',
+      rating: 4.8,
+      images: [
+        'https://m.media-amazon.com/images/I/81MJhRednWL._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/81w-ZmU+CQL._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/81ln5q7YiCL._AC_SL1500_.jpg'
+      ]
+    },
+    {
+      asin: 'B08CDCYJ8P',
+      name: 'MAGCOMSEN Men\'s Jacket Lightweight Bomber Jacket Casual Windbreaker Jackets Windproof Zip Up Coats with 5 Pockets',
+      description: 'OCCASIONS: This lightweight jackets perfects for daily wear or outdoor activity, such as business casual, street fashion, party, travel, etc; Suitable for spring fall and winter.',
+      rating: 4.5,
+      images: [
+        'https://m.media-amazon.com/images/I/71Kj3KbQdYL._AC_SX569_.jpg',
+        'https://m.media-amazon.com/images/I/71fWLXp1NPL._AC_SY550_.jpg',
+        'https://m.media-amazon.com/images/I/81ho7CeXzrL._AC_SX569_.jpg'
+      ]
+    },
+    {
+      asin: 'B07G3S2G2D',
+      name: 'SaphiRose Women\'s Long Hooded Rain Jacket Outdoor Raincoat Windbreaker',
+      description: 'This trench coat is suitable for daily wear, camping, hiking, riding, cycling, climbing, picnic, walking, traveling, walking the dog, watching ball games, and other outdoor activities and sports.',
+      rating: 4.6,
+      images: [
+        'https://m.media-amazon.com/images/I/61Jkl9493rL._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/81x0HMH8biL._AC_SL1500_.jpg',
+        'https://m.media-amazon.com/images/I/61LtDj4G4XL._AC_SL1500_.jpg'
       ]
     },
   ];
